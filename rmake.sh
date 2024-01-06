77677aacb99d7ead4f07374a84a2ee1a08664301
TymekBrunka <tymkopolo@wp.pl>
2024-01-06 16:32:09 +0100

    hej

---
+++ rmake.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+read -p "Commit message (blank for no commit): " msg
+cp ./config.def.h ./config.h
+sudo make clean install
+if [[ -n $msg ]] ; then
+    echo "jej"
+    git add .
+    git commit -m $msg
+fi
